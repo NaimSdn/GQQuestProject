d44a8af97c51889dd69abf5189786ebee401955e
NaimSdn <saadoune.naim.pro@gmail.com>
2024-09-26 18:51:47 +0200

    Update gun and bullet physics

--- gun.gd
+++ gun.gd
@@ -9,6 +9,10 @@ func _physics_process(delta: float) -> void:
 func shoot():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
-	
 	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
